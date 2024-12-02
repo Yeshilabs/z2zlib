b43740ded3cd3f151c961c5e406b8b93b1ab5b3a
YofiY <theburnface2013@gmail.com>
2024-12-02 17:16:33 +0100

    test configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ["src/**/**/*.test.ts", "src/**/**/*.spec.ts"], // Adjust to match your file pattern
     globals: true,
     environment: "node",
   },
